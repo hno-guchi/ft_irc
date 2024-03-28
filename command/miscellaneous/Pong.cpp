ce6f0e4792fc6496eace65699795345c6e3c8e23
hnoguchi <>
2024-03-28 17:45:42 +0900

    実装するコマンドのファイルを作成。

---
+++ command/miscellaneous/Pong.cpp
@@ -0,0 +1,12 @@
+/*
+ *    Command: PONG
+ * Parameters: <server> [ <server2> ]
+ *
+ * Numeric Replies:
+ *             409 ERR_NOORIGIN     ":No origin specified"
+ *             402 ERR_NOSUCHSERVER "<server name> :No such server"
+ *
+ * Example:
+ *    PONG csd.bu.edu tolsun.oulu.fi  ; PONG message from csd.bu.edu to tolsun.oulu.fi
+ *
+ */
---
+++ command/sending/Notice.cpp
@@ -0,0 +1,4 @@
+/*
+ *    Command: NOTICE
+ * Parameters: <msgtarget> <text>
+ */
